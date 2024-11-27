671d730c6654c92004f253c557a29124c9f19351
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2024-11-27 16:23:08 +0100

    added the bench replacement area, and restyled the inputs to be two inputs in one row, also made the name and the photo of the player get changed dynamicaly from the first two inputs name and photo

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,40 @@
 let goalKeeperInputs = document.getElementById("goalkeeper-only-inputs")
 let playersInputs = document.getElementById("players-only-inputs");
-let position = document.getElementById("player-position")
+
+
+// ------------------Declaring input variables---------------------------
+
+let nameInput = document.getElementById("player-name-input");
+let photoInput = document.getElementById("player-photo")
+let position = document.getElementById("player-position");
+let addPlayerBtn = document.getElementById("add-player-btn");
+
+// ---------Add event listeners to elements inside the document----------
+addPlayerBtn.addEventListener("click",()=>{
+    let playerCard = document.createElement("div");
+    playerCard.innerHTML= `
+    <div class="  translate-y-4 w-3/5 aspect-auto">
+                <img src="${photoInput.value}" alt="">
+            </div>
+            <h2 class="z-10 font-medium text-sm">${nameInput.value}</h2>
+            <div class="w-4/5 h-fit flex flex-row gap-1">
+                <span class="text-vs font-medium text-center"><p class="text-center">PAC</p> <p class="text-center">99</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">SHO</p> <p class="text-center">99</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">PAS</p> <p class="text-center">99</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">DRI</p> <p class="text-center">99</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">DEF</p> <p class="text-center">99</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">PHY</p> <p class="text-center">99</p></span>
+            </div>
+
+            <div class="flex flex-row gap-4">
+                <img class="h-4 aspect-auto " src="https://cdn.sofifa.net/flags/ar.png" alt="">
+                <img class="h-4 aspect-auto" src="https://cdn.sofifa.net/meta/team/239235/120.png" alt="">
+            </div>
+    `
+let rightwing= document.getElementById("right-wing");
+rightwing.innerHTML = playerCard.innerHTML;
+nameInput.value = "";
+})
 
 position.addEventListener('change',()=>{
 
