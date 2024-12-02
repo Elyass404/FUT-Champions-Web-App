3ca346fc90e479c3d9fe1a7733fefcaa5381ed26
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2024-12-02 01:26:58 +0100

    finished the information modification functionality to the players presented in the field and making it change the position of the player if its original position has been changed also

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,4 @@
+
 let goalKeeperInputs = document.getElementById("goalkeeper-only-inputs")
 let playersInputs = document.getElementById("players-only-inputs");
 let flags = {
@@ -51,7 +52,19 @@ if(JSON.parse(localStorage.getItem("positionValability"))){
     }
     localStorage.setItem("positionValability",JSON.stringify(positionValability))
 }
-// -----------Declaring position of the players in the field 
+
+// -----------functions building------------------
+
+function resetInputs(){
+    let allInputs = document.querySelectorAll('input, select');
+allInputs.forEach((input)=>{
+    input.value="";
+    playersInputs.classList.add("hidden");
+    goalKeeperInputs.classList.add("hidden");
+})
+}
+
+// -----------Declaring position of the players in the field--------
 let goalKeeperPos = document.getElementById("goal-keeper");
 let leftCenterBackPos = document.getElementById("left-center-back");
 let leftBackPos = document.getElementById("left-back");
@@ -66,6 +79,7 @@ let rightWingPos = document.getElementById("right-wing");
 
 // ------------------Declaring input variables---------------------------
 
+let idInput = document.getElementById("id-input");
 let nameInput = document.getElementById("player-name-input");
 let photoInput = document.getElementById("player-photo")
 let position = document.getElementById("player-position");
@@ -88,6 +102,8 @@ let speed = document.getElementById("speed");
 let positioning = document.getElementById("positioning");
 
 let addPlayerBtn = document.getElementById("add-player-btn");
+let updatePlayerBtn = document.getElementById("update-player-btn");
+let cancelUpdatePlayerBtn = document.getElementById("cancel-update-player-btn");
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
@@ -95,35 +111,63 @@ document.addEventListener("DOMContentLoaded", ()=>{
         playersArr = JSON.parse(localStorage.getItem("players"));
         console.log(playersArr);
         window.alert("rah kaaaaaaaaaayn dak tableau alm3lem");
-
-        let gkOnField = playersArr.gkArray.findIndex(gk => gk.onField==true);
+        let gkArray = playersArr.gkArray;
+        let gkOnField = gkArray.findIndex(gk => gk.onField==true);
         
         
         if(gkOnField > -1){
             goalKeeperPos.innerHTML =`
         <div class="absolute top-8 left-5 flex flex-col gap-0">
-                <div class="text-center text-xl font-bold">${playersArr.gkArray[gkOnField].rating}</div>
-                <div class="text-center text-xs font-medium">${playersArr.gkArray[gkOnField].position.toUpperCase()}</div>
+                <div class="text-center text-xl font-bold">${gkArray[gkOnField].rating}</div>
+                <div class="text-center text-xs font-medium">${gkArray[gkOnField].position.toUpperCase()}</div>
             </div>
 
     <div class="translate-y-4 w-3/5 aspect-auto">
-                <img src="${playersArr.gkArray[gkOnField].photo}" alt="">
+                <img src="${gkArray[gkOnField].photo}" alt="">
             </div>
-            <h2 class="z-10 font-medium text-sm">${playersArr.gkArray[gkOnField].name}</h2>
+            <h2 class="z-10 font-medium text-sm">${gkArray[gkOnField].name}</h2>
             <div class="w-4/5 h-fit flex flex-row gap-1">
-                <span class="text-vs font-medium text-center"><p class="text-center">DIV</p> <p class="text-center">${playersArr.gkArray[gkOnField].diving}</p></span>
-                <span class="text-vs font-medium text-center"><p class="text-center">HAN</p> <p class="text-center">${playersArr.gkArray[gkOnField].handling}</p></span>
-                <span class="text-vs font-medium text-center"><p class="text-center">KIC</p> <p class="text-center">${playersArr.gkArray[gkOnField].kicking}</p></span>
-                <span class="text-vs font-medium text-center"><p class="text-center">REF</p> <p class="text-center">${playersArr.gkArray[gkOnField].reflexes}</p></span>
-                <span class="text-vs font-medium text-center"><p class="text-center">SPD</p> <p class="text-center">${playersArr.gkArray[gkOnField].speed}</p></span>
-                <span class="text-vs font-medium text-center"><p class="text-center">POS</p> <p class="text-center">${playersArr.gkArray[gkOnField].positioning}</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">DIV</p> <p class="text-center">${gkArray[gkOnField].diving}</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">HAN</p> <p class="text-center">${gkArray[gkOnField].handling}</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">KIC</p> <p class="text-center">${gkArray[gkOnField].kicking}</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">REF</p> <p class="text-center">${gkArray[gkOnField].reflexes}</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">SPD</p> <p class="text-center">${gkArray[gkOnField].speed}</p></span>
+                <span class="text-vs font-medium text-center"><p class="text-center">POS</p> <p class="text-center">${gkArray[gkOnField].positioning}</p></span>
             </div>
 
             <div class="flex flex-row gap-4">
-                <img class="h-4 aspect-auto " src="${flags[playersArr.gkArray[gkOnField].nationality]}" alt="">
-                <img class="h-4 aspect-auto" src="${clubs[playersArr.gkArray[gkOnField].club]}" alt="">
+                <img class="h-4 aspect-auto " src="${flags[gkArray[gkOnField].nationality]}" alt="">
+                <img class="h-4 aspect-auto" src="${clubs[gkArray[gkOnField].club]}" alt="">
             </div>
+            <button class = "absolute modifyBtn">modify
+            </button>
         `
+        let modifyBtn = goalKeeperPos.querySelector(".modifyBtn");
+        modifyBtn.addEventListener('click',()=>{
+            addPlayerBtn.classList.add("hidden");
+            updatePlayerBtn.classList.remove("hidden");
+            cancelUpdatePlayerBtn.classList.remove("hidden");
+
+            window.alert(`hello this is the id of the player : ${gkArray[gkOnField].id}`);
+
+            goalKeeperInputs.classList.remove("hidden");
+            playersInputs.classList.add("hidden");
+
+            idInput.value = gkArray[gkOnField].id;
+            nameInput.value = gkArray[gkOnField].name;
+            photoInput.value = gkArray[gkOnField].photo;
+            nationality.value = gkArray[gkOnField].nationality;
+            position.value = gkArray[gkOnField].position;
+            club.value = gkArray[gkOnField].club;
+            rating.value = gkArray[gkOnField].rating;
+            diving.value = gkArray[gkOnField].diving;
+            handling.value = gkArray[gkOnField].handling;
+            kicking.value = gkArray[gkOnField].kicking;
+            reflexes.value = gkArray[gkOnField].reflexes;
+            speed.value = gkArray[gkOnField].speed;
+            positioning.value = gkArray[gkOnField].positioning;
+        });
+        
         }
 
         let positions = {
@@ -139,10 +183,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
             lw: { element: leftWingPos, array: playersArr.lwArray }
         };
 
-        for (let position in positions) {
-            // let { element, array } = positions[position];
-            let element = positions[position].element;
-            let array = positions[position].array;
+        for (let post in positions) {
+           
+            let element = positions[post].element;
+            let array = positions[post].array;
             let playerIndex = array.findIndex(player => player.onField == true);
             if (playerIndex > -1) {
                 element.innerHTML =`
@@ -168,10 +212,40 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 <img class="h-4 aspect-auto " src="${flags[array[playerIndex].nationality]}" alt="">
                 <img class="h-4 aspect-auto" src="${clubs[array[playerIndex].club]}" alt="">
             </div>
+
+            <button class = "absolute modifyBtn">modify
+            </button>
         `
+        let modifyBtn = element.querySelector(".modifyBtn");
+            modifyBtn.addEventListener('click',()=>{
+                addPlayerBtn.classList.add("hidden");
+                updatePlayerBtn.classList.remove("hidden");
+                cancelUpdatePlayerBtn.classList.remove("hidden");
+
+                window.alert(`hello this is the id of the player : ${array[playerIndex].id}`);
+
+                goalKeeperInputs.classList.add("hidden");
+                playersInputs.classList.remove("hidden");
+
+                idInput.value = array[playerIndex].id;
+                nameInput.value = array[playerIndex].name;
+                photoInput.value = array[playerIndex].photo;
+                nationality.value = array[playerIndex].nationality;
+                position.value = array[playerIndex].position;
+                club.value = array[playerIndex].club;
+                rating.value = array[playerIndex].rating;
+                pace.value = array[playerIndex].pace;
+                shooting.value = array[playerIndex].shooting;
+                passing.value = array[playerIndex].passing;
+                dribbling.value = array[playerIndex].dribbling;
+                defending.value = array[playerIndex].defending;
+                physical.value = array[playerIndex].physical;
+
+            });
             }
+            
         }
- 
+
     }else{
         window.alert("rah makaynch dak tableau alm3lem");
         
@@ -191,11 +265,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
         localStorage.setItem("players",JSON.stringify( playersArr));
     };
 
+
+
 });
 
 // ---------Add event listeners to elements inside the document----------
 addPlayerBtn.addEventListener("click",()=>{
-   
+    idInput.value = Date.now().toString();
     let playerCard= `
     <div class="absolute top-8 left-5 flex flex-col gap-0">
                 <div class="text-center text-xl font-bold">${rating.value}</div>
@@ -240,6 +316,8 @@ addPlayerBtn.addEventListener("click",()=>{
                 <img class="h-4 aspect-auto " src="${flags[nationality.value]}" alt="">
                 <img class="h-4 aspect-auto" src="${clubs[club.value]}" alt="">
             </div>
+
+            
             
         `;
 
@@ -311,6 +389,7 @@ addPlayerBtn.addEventListener("click",()=>{
 }
 
 let playerObj = {
+    id: idInput.value,
     name : nameInput.value, 
     photo : photoInput.value,
     position : position.value,
@@ -356,13 +435,7 @@ localStorage.setItem("positionValability",JSON.stringify(positionValability));
 
 
 // To reset all the inputs of the form 
-
-let allInputs = document.querySelectorAll('input, select');
-allInputs.forEach((input)=>{
-    input.value="";
-})
-
-
+resetInputs();
 
 })
 
@@ -377,3 +450,116 @@ position.addEventListener('change',()=>{
         playersInputs.classList.remove("hidden");
     }
 })
+
+
+updatePlayerBtn.addEventListener("click",()=>{
+   let playersArr= JSON.parse(localStorage.getItem("players"));
+   let positionValability= JSON.parse(localStorage.getItem("positionValability"));
+   console.log("this is the result of clicking the update button")
+   console.log(playersArr);
+
+   let array;
+   let playerIndex;
+   let playerOriginalPosition;
+
+   for(let post in playersArr){
+    let index = playersArr[post].findIndex(player => player.id == idInput.value);
+    if (index > -1){
+        array = playersArr[post];
+        playerIndex = index ;
+        playerOriginalPosition = array[playerIndex].position; 
+        break;
+    }
+   }
+
+   console.log(array);
+   console.log(playerIndex);
+   console.log(playerOriginalPosition);
+
+   if(playerOriginalPosition == position.value){
+    array[playerIndex].name = nameInput.value;
+    array[playerIndex].photo = photoInput.value
+    array[playerIndex].position = position.value
+    array[playerIndex].nationality = nationality.value
+    array[playerIndex].club = club.value
+    array[playerIndex].rating = rating.value
+    if (position.value =="gk"){
+        array[playerIndex].diving = diving.value
+        array[playerIndex].handling = handling.value
+        array[playerIndex].kicking = kicking.value
+        array[playerIndex].reflexes = reflexes.value
+        array[playerIndex].speed = speed.value
+        array[playerIndex].positioning = positioning.value
+    }else{
+        array[playerIndex].pace = pace.value
+        array[playerIndex].shooting = shooting.value
+        array[playerIndex].passing = passing.value
+        array[playerIndex].dribbling = dribbling.value
+        array[playerIndex].defending = defending.value
+        array[playerIndex].physical = physical.value
+    }
+   }else{
+    if (array[playerIndex].onField == true){
+        positionValability[array[playerIndex].position] = "free";
+    }
+    array.splice(playerIndex,1);
+    let playerObj= {
+        id: idInput.value,
+        name: nameInput.value,
+        photo: photoInput.value,
+        position: position.value,
+        nationality: nationality.value,
+        club: club.value,
+        rating:rating.value
+    }
+    if(position.value == "gk"){
+        playerObj = {...playerObj,
+        diving : diving.value,
+        handling : handling.value,
+        kicking : kicking.value,
+        reflexes : reflexes.value,
+        speed : speed.value,
+        positioning : positioning.value,
+        }
+        if (positionValability[position.value]== "free"){
+            playerObj= {...playerObj,
+                onField : true
+            }
+            positionValability[position.value]= "full";
+        }else{
+            playerObj= {...playerObj,
+                onField : false
+            }
+        }
+    }else{
+        playerObj = {...playerObj,
+        pace: pace.value,
+        shooting: shooting.value,
+        passing: passing.value,
+        dribbling: dribbling.value,
+        defending: defending.value,
+        physical: physical.value,
+        }
+        if (positionValability[position.value]== "free"){
+            playerObj= {...playerObj,
+                onField : true
+            }
+            positionValability[position.value]= "full";
+        }else{
+            playerObj= {...playerObj,
+                onField : false
+            }
+        }
+    }
+
+    playersArr[`${position.value}Array`].push(playerObj);
+
+   }
+
+   localStorage.setItem("players",JSON.stringify(playersArr));
+   localStorage.setItem("positionValability",JSON.stringify(positionValability));
+   resetInputs();
+
+
+})
+
